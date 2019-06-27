06a1fc0731e5b23bd26392d16aa53fd0fa196f7a
harnoorBandesh <harnoor_04@hotmail.com>
2019-06-28 02:18:52 +0530

    initial comit for getsnapshotbeforeupdate and component did update

--- src/components/lifeCycleMethodsComponent.jsx
+++ src/components/lifeCycleMethodsComponent.jsx
@@ -3,9 +3,20 @@ import React from "react";
 export class LifeCycleMethods extends React.Component {
     constructor(props) {
         super(props);
+        this.ref = React.createRef();
+        this.height = 100;
         this.state = {
-            derivedValue: ""
+            derivedValue: "",
+            heightChangeCount: 0
         }
+    }
+
+    handleClick = () => {
+        this.setState((state) => {
+            return {
+                heightChangeCount: state.heightChangeCount + 1
+            }
+        });
     }
 
     static getDerivedStateFromProps = (props,state) => {
@@ -16,10 +27,31 @@ export class LifeCycleMethods extends React.Component {
         }
     }
 
+    // getSnapshotBeforeUpdate = (prevProps, prevState) => {
+    //     if(prevState.heightChangeCount !== this.state.heightChangeCount) {
+    //         return this.ref.;
+    //     }
+    //     return null;
+    // }
+
+    // componentDidUpdate(prevProps,prevState,snapshot) {
+    //     if (snapshot !== null) {
+    //         this.height
+    //     }
+    // }
+
     render() {
         return(
             <React.Fragment>
                 <p>The derived state value is : {this.state.derivedValue}</p>
+                <button type="button" onClick={this.handleClick}>Click Me</button>
+                <p>You have changed the height of DIV by: {this.state.heightChangeCount} times</p>
+                <div ref={this.ref} style={{
+                    width: '100px',
+                    height: this.height,
+                    backgroundColor: 'white'
+                }}>
+                </div>
             </React.Fragment>
         )
     }
